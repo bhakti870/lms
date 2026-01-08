fed728c044c664c8a35c17956b9a9da59f934030
bhakti870 <patelbhakti636@gmail.com>
2026-01-08 11:35:43 +0530

    cdn links order changes]

--- resources/views/frontend/section/link.blade.php
+++ resources/views/frontend/section/link.blade.php
@@ -6,24 +6,38 @@
 <!-- Google fonts -->
 <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link href="https://fonts.googleapis.com/css2?family=Outfit:wght@400;500;600;700&family=Merriweather:wght@700;900&family=Roboto:wght@400;500&display=swap" rel="stylesheet">
+<link
+    href="https://fonts.googleapis.com/css2?family=Outfit:wght@400;500;600;700&family=Merriweather:wght@700;900&family=Roboto:wght@400;500&display=swap"
+    rel="stylesheet">
 
 <!-- Favicon -->
 <link rel="icon" sizes="16x16" href="{{asset('frontend/images/favicon.png')}}">
 
 <!-- inject:css -->
+<!--
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
 <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
 <link rel="stylesheet" href="{{ asset('frontend/css/line-awesome.css') }}">
-<link rel="stylesheet" href="{{ asset('frontend/new_design/style.css') }}">
+<link rel="stylesheet" href="{{ asset('frontend/new_design/style.css') }}"> -->
 
 <!-- Keep some essential LMS styles if needed, but the new design should take precedence -->
 <!-- <link rel="stylesheet" href="{{ asset('frontend/css/owl.carousel.min.css') }}"> -->
 <!-- <link rel="stylesheet" href="{{ asset('frontend/css/owl.theme.default.min.css') }}"> -->
+<!--
 <link rel="stylesheet" href="{{ asset('frontend/css/style.css') }}"> 
-<link rel="stylesheet" href="{{ asset('frontend/css/header-dropdown.css') }}"> 
+<link rel="stylesheet" href="{{ asset('frontend/css/header-dropdown.css') }}"> -->
+
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
+<link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
+
+<link rel="stylesheet" href="{{ asset('frontend/css/line-awesome.css') }}">
+<link rel="stylesheet" href="{{ asset('frontend/css/style.css') }}">
+<link rel="stylesheet" href="{{ asset('frontend/css/header-dropdown.css') }}">
+
+<!-- NEW DESIGN ALWAYS LAST -->
+<link rel="stylesheet" href="{{ asset('frontend/new_design/style.css') }}">
 
 <!-- @stack('css') -->
 <!-- end inject -->
-
