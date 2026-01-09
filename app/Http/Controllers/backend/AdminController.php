f1b6217137f403543dc7b1d30b5004125624724b
Bhakti_Bhut_17 <141489733+bhakti870@users.noreply.github.com>
2026-01-09 17:11:48 +0530

    Refactor authentication logic in AdminController
    Removed unnecessary authentication and session regeneration code.

--- app/Http/Controllers/backend/AdminController.php
+++ app/Http/Controllers/backend/AdminController.php
@@ -36,12 +36,6 @@ class AdminController extends Controller
             'email' => ['required', 'email'],
             'password' => ['required'],
         ]);
-                       $request->authenticate();
-
-        $request->session()->regenerate();
-
-        $user = $request->user();
-        dd($user);
 
         if (Auth::attempt($credentials, $request->boolean('remember'))) {
             $user = Auth::user();
